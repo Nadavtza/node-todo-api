236162c577b3b43c0d24f283f965d6535666e293
Nadavtza <bscmcrh@gmail.com>
2018-06-11 22:03:58 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 //library exports
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 //Local exports
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
 
 
 var app = express();
@@ -31,6 +33,27 @@ app.get('/todos' , (req , res) => {
         res.status(400).send(e);
     });
 });
+
+app.get('/todos/:id' , (req , res) => {
+    var id = req.params.id ;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send({message:'ID not valid'});
+    }
+
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            res.status(404).send({message:'ID not found'});
+        }
+        res.send({message: 'Todo:',
+        todo});
+    } , (e) =>{
+        res.status(400).send();
+    });
+});
+
+
+
 
 app.listen(3000 , ()=>{
     console.log('Started on port 3000');
