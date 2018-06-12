bedbd38eabc70fd50bfb62e22d25e3c36d05983e
Nadavtza <bscmcrh@gmail.com>
2018-06-12 10:50:07 +0300

    change mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-let db = {
-    localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://nadav99:prl999@ds149268.mlab.com:49268/todoapp'
-  };
-mongoose.connect( db.mlab || db.localhost);
+// let db = {
+//     localhost: 'mongodb://localhost:27017/TodoApp',
+//     mlab: 'mongodb://nadav99:prl999@ds149268.mlab.com:49268/todoapp'
+//   };
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 
 
